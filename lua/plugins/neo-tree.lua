d41384410b21dea636f698bc80e95be48b008246
TristanD0703 <tristan.desoto04@gmail.com>
2025-05-16 09:01:24 -0500

    start

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+---@diagnostic disable: undefined-global
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim"
+  },
+  config = function()
+    vim.keymap.set("n", '<leader>b', ":Neotree filesystem reveal left<CR>", {})
+  end
+}
+
