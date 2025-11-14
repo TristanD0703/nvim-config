09cbc124b02f22e2df9de677a85c2cddb816b02d
TristanD0703 <tristan.desoto04@gmail.com>
2025-11-14 15:23:26 -0600

    some changes to neotree and none-ls

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,7 +3,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "prettier", "pyright", "ts_ls" },
+        auto_install=true
 			})
 		end,
 	},
@@ -22,7 +22,6 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
-
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,10 +6,20 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    "3rd/image.nvim"
+    "3rd/image.nvim",
   },
   config = function()
-    vim.keymap.set("n", '<leader>b', ":Neotree filesystem reveal left<CR>", {})
-  end
+    vim.keymap.set("n", "<leader>p", ":Neotree filesystem reveal right<CR>", {})
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      window = {
+        position = "right",
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    })
+  end,
 }
-
